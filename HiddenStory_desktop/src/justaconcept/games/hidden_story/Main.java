13236b681fdd81e147212a74d0d6f132b0afce80
Adam Streck <adam.streck@gmail.com>
2013-07-18 13:44:33 +0200

    Initial commit.

---
+++ HiddenStory_desktop/src/justaconcept/games/hidden_story/Main.java
@@ -0,0 +1,18 @@
+package justaconcept.games.hidden_story;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+	JFrame frame = new JFrame();
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setResizable( false );
+	HiddenStory hidden_story = new HiddenStory();
+	frame.getContentPane().add(hidden_story);
+	frame.setVisible(true);
+	hidden_story.init();
+	int width = hidden_story.sketchWidth();
+	int height = hidden_story.sketchHeight();
+	frame.setSize(width, height);
+    }
+}
